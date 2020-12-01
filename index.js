6ebd76c6472269041a3348760f69ab44c2138729
Leela-Madhavi <66566161+Leela-Madhavi@users.noreply.github.com>
2020-12-01 17:33:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+$(function() {
+   
+    $(".form-control").on('focus', function(){
+
+        $(this).parents(".form-group").addClass('focused');
+
+    });
+
+    $(".form-control").on('blur', function(){
+
+        $(this).parents(".form-group").removeClass('focused');
+
+    });
+
+});
